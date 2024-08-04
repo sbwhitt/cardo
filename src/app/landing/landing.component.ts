ebb9cef34d9ae73194880f4c4b85673e328c0568
Samuel Whittenberger <sbw98978@gmail.com>
2024-08-03 23:53:38 -0400

    wip landing

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,17 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { AuthService } from '../services/auth.service';
 import { MenuService } from '../services/menu.service';
 
+interface MenuOption {
+  link: string;
+  color: string;
+}
+
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterLink, NavbarComponent],
+  imports: [CommonModule, RouterLink, NavbarComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
+  menuOptions: MenuOption[] = [
+    {
+      link: 'list',
+      color: 'lightgreen'
+    },
+    {
+      link: 'deck',
+      color: 'plum'
+    },
+    {
+      link: 'sets',
+      color: 'palegoldenrod'
+    }
+  ];
+
   constructor(
     private authService: AuthService,
     private menuService: MenuService
